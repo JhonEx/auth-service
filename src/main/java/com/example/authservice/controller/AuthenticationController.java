6e64dbbaa53a11a23d92bcf66f654b56194f1f0a
Jhon Ramirez <sir.edwards@gmail.com>
2025-06-19 11:11:42 -0500

    Add token login and user management endpoints

--- src/main/java/com/example/authservice/controller/AuthenticationController.java
+++ src/main/java/com/example/authservice/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,13 @@ public class AuthenticationController {
         return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
     }
 
+    @PostMapping("/login-for-token")
+    public ResponseEntity<?> loginForToken(@RequestBody LoginRequestDTO loginRequest) {
+        log.info("Authentication API: login-for-token requested by user: {}", loginRequest.getUsername());
+        AuthResponseDTO authResponseDTO = authenticationService.loginForToken(loginRequest);
+        return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
+    }
+
     @PostMapping("/create-user")
     public ResponseEntity<?> createUser(@Valid @RequestBody RegisterUserRequestDTO registerUserRequestDTO) {
         log.info("Authentication API: createUser: ", registerUserRequestDTO.getEmail());
@@ -45,5 +53,17 @@ public class AuthenticationController {
             throw new BadRequestException(AppExceptionConstants.USER_RECORD_FOUND);
         }
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/refresh-token")
+    public ResponseEntity<?> refreshToken() {
+        AuthResponseDTO authResponseDTO = authenticationService.refreshToken();
+        return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers() {
+        List<UserDTO> users = authenticationService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/authservice/service/auth/AuthenticationService.java
+++ src/main/java/com/example/authservice/service/auth/AuthenticationService.java
@@ -11,6 +11,26 @@ public interface AuthenticationService {
 
     AuthResponseDTO loginUser(LoginRequestDTO loginRequest);
 
+    /**
+     * Additional login method that simply returns the JWT token.
+     * This can be used by clients that only need a token to access
+     * secured resources.
+     */
+    AuthResponseDTO loginForToken(LoginRequestDTO loginRequest);
+
+    /**
+     * Returns a fresh JWT token for the currently authenticated user.
+     */
+    AuthResponseDTO refreshToken();
+
+    /**
+     * Creates a new user in the system.
+     */
     UserDTO createUser(RegisterUserRequestDTO registerUserRequestDTO) throws SQLIntegrityConstraintViolationException;
+
+    /**
+     * Returns all users in the system.
+     */
+    java.util.List<UserDTO> getAllUsers();
 
 }
